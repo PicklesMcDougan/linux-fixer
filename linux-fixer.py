e2e323b79d45286417acf39a3c2f2df8713e05d0
John Castranio <jcastran@redhat.com>
2024-11-20 12:02:08 -0500

    Adding python

---
+++ linux-fixer.py
@@ -0,0 +1,13 @@
+#!/bin/python
+import sys
+from time import sleep
+
+def progress_bar():
+    for i in range(21):
+        sys.stdout.write('\r')
+        sys.stdout.write("[%-20s] %d%%" % ('='*i, 5*i))
+        sys.stdout.flush()
+        sleep(.25)
+    
+progress_bar()
+print ("\nProblems Resolved")
